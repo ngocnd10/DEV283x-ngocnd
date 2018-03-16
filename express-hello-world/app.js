dcebb56c7011861546dc51b070bb31e36c8b8801
Ngoc ND <ngocnd10@gmail.com>
2018-03-17 00:10:27 +0700

    Implementing REST API Routing

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -1,10 +1,36 @@
-// Imports
+// // Imports
+// const express = require('express')
+// // Instantiations
+// const app = express()
+// // Routes
+// app.get('/', (req, res) => {
+//     res.send({msg:'hello world'})
+// })
+// // Bootup
+// app.listen(3000)
+
 const express = require('express')
-// Instantiations
 const app = express()
-// Routes
-app.get('/', (req, res) => {
-    res.send({msg:'hello world'})
+
+const profile = {
+    username: 'azat',
+    email: '[reducted]',
+    url: 'http://azat.co'
+}
+app.get('/profile', (req, res)=>{
+    res.send(profile)
 })
-// Bootup
+app.post('/profile', (req, res) => {
+    profile = req.body
+    res.sendStatus(201)
+})
+app.put('/profile', (req, res)=>{
+    Object.assign(profile, req.body)
+    res.sendStatus(204)
+})
+app.delete('/profile', (req, res)=>{
+    profile ={}
+    res.sendStatus(204)
+})
+
 app.listen(3000)
