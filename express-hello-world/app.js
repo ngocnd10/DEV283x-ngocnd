53fda77ac1d3ecf733e68788af37babd3dcb2149
Ngoc ND <ngocnd10@gmail.com>
2018-03-18 18:13:24 +0700

    URL Parameters, Query Strings and Input Validation

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -11,25 +11,37 @@
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-const profile = {
+app.use(bodyParser.json())
+
+const profile = [{
     username: 'azat',
     email: '[reducted]',
     url: 'http://azat.co'
-}
+}]
 app.get('/profile', (req, res)=>{
+    if (req.query.id) return res.send(profile[req.query.id])
     res.send(profile)
 })
 app.post('/profile', (req, res) => {
-    profile = req.body
+    if (!req.body.first_name.trim() || !req.body.last_name.trim()) return res.sendStatus(400)
+    let obj = {
+        first_name:  req.body.first_name,
+        last_name: req.body.last_name
+    }
+    profile.push(req.body)
+    console.log('created', profile)
     res.sendStatus(201)
 })
-app.put('/profile', (req, res)=>{
-    Object.assign(profile, req.body)
+app.put('/profile/:id', (req, res)=>{
+    Object.assign(profile[req.params.id], req.body)
+    console.log('updated', profile[req.params.id])
     res.sendStatus(204)
 })
-app.delete('/profile', (req, res)=>{
-    profile ={}
+app.delete('/profile/:id', (req, res)=>{
+    profile.splice(req.params.id, 1)
+    console.log('deleted', profile)
     res.sendStatus(204)
 })
 
